80cc528105111e1fab33d4e196486cd4199cc82c
hgokhru101 <hgokhru101@gmail.com>
2021-02-05 22:16:57 +0530

    Added News RSS Feed

---
+++ js/medicine_feed.js
@@ -0,0 +1,26 @@
+let url = "https://www.indiaeducation.net/rss/alertsmedical.xml";
+const textarea = document.querySelector("#feed-textarea > ul");
+
+feednami.load(url).then((feed) => {
+  textarea.value = "";
+  console.log(feed.entries);
+  for (let entry of feed.entries) {
+    //create a list element
+    let li = document.createElement("li");
+    //add HTML content to list items
+    li.innerHTML = `<div class="card">
+					<div class="card-body">
+						<h5 class="card-title">
+            <a href="${entry.link}">${entry.title}</a>
+            </h5>
+						<p class="card-text">
+            <p class="mb-0 mt-2 font-italic">${entry.description}</p>
+            <br>
+						<h6>Publish Date: ${entry.pubDate} </h6>
+						</p>
+					</div>
+				</div>`;
+    //append HTML content to list
+    textarea.appendChild(li);
+  }
+});
